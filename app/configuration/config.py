8c427e69be1cda2cc854f30f8c526ebc96f9473b
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-19 19:27:06 -0300

    Update config.py.
    Added attribute function for defining the full database url in just one place and added comments.

--- app/configuration/config.py
+++ app/configuration/config.py
@@ -5,16 +5,23 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Settings(BaseSettings):
 
+    # Database
     database_host: str
-    database_port: str
+    database_port: int
     database_user: str
     database_pass: str
     database_name: str
 
+    # Application
     debug: bool = False
+    log_level: str = 'INFO'
 
     class Config:
-        env_file = os.path.join(BASE_DIR, ".env")
+        env_file = os.path.join(BASE_DIR, '.env')
         env_file_encoding = 'utf-8'
+
+    @property
+    def database_url(self: object) -> str:
+        return f'postgresql+asyncpg://{self.database_user}:{self.database_pass}@{self.database_host}:{self.database_port}/{self.database_name}'
 
 settings = Settings()
