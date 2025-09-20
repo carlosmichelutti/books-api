8db0c4fff65d90ca9a5aa6b89e412afbbd2eeb3d
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-20 15:59:19 -0300

    Update session.py
    Added return type of get_sessionn function.

--- app/database/session.py
+++ app/database/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from typing import AsyncGenerator
 
 from configuration.config import settings
 
@@ -7,6 +8,6 @@ engine = create_async_engine(url=settings.database.database_url, echo=settings.a
 
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-async def get_session():
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         yield session
