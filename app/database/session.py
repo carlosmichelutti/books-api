6fbaa15afd2c114b148be31ecac9fe69afa4d865
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-20 15:39:57 -0300

    Update session.py
    Removal of the definition of the database url constant, modularizing this coming directly from the settings module and also the definition of whether the execution mode is debug or not.

--- app/database/session.py
+++ app/database/session.py
@@ -1,13 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from configuration.config import settings
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = f'postgresql+asyncpg://{settings.database_user}:{settings.database_pass}@{settings.database_host}:{settings.database_port}/{settings.database_name}'
+from configuration.config import settings
 
-engine = create_async_engine(url=DATABASE_URL, echo=False)
+engine = create_async_engine(url=settings.database.database_url, echo=settings.app.debug)
 
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-async def get_db():
+async def get_session():
     async with SessionLocal() as session:
         yield session
