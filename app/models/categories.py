7fc33dd85edf60785c1f49e7d6b403fa4741b9c3
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-20 16:09:21 -0300

    Update categories.py
    Removal of the callable for column data types and creation of the __repr__ function to represent the object when instantiated to facilitate logging and debugging in the future.

--- app/models/categories.py
+++ app/models/categories.py
@@ -11,4 +11,7 @@ class VwCategories(Base):
     }
     id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
     category = Column(String(50), unique=False, nullable=False)
-    quantity_books = Column(Integer(), unique=False, nullable=False)
+    quantity_books = Column(Integer, unique=False, nullable=False)
+
+    def __repr__(self: object) -> str:
+        return f'<VwCategories(category={self.category}, quantity_books={self.quantity_books})>'
