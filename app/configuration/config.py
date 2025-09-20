629444519b9752ead8f44c45deb133eff53878ac
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-20 14:49:27 -0300

    Update config.py
    Separation of the Settings class into two other configuration classes, one to configure the database and another to configure the fastapi application, and another class to combine the database and application configuration classes into just one class for modularization.

--- app/configuration/config.py
+++ app/configuration/config.py
@@ -1,27 +1,48 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+from typing import Literal
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class Settings(BaseSettings):
+class DatabaseSettings(BaseSettings):
 
     # Database
-    database_host: str
-    database_port: int
-    database_user: str
-    database_pass: str
-    database_name: str
-
-    # Application
-    debug: bool = False
-    log_level: str = 'INFO'
-
-    class Config:
-        env_file = os.path.join(BASE_DIR, '.env')
-        env_file_encoding = 'utf-8'
+    host: str
+    port: int
+    user: str
+    password: str
+    name: str
+    
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, '.env'),
+        env_file_encoding='utf-8',
+        env_prefix='DATABASE_',
+        extra='ignore',
+    )
 
     @property
     def database_url(self: object) -> str:
-        return f'postgresql+asyncpg://{self.database_user}:{self.database_pass}@{self.database_host}:{self.database_port}/{self.database_name}'
+        return f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}'
+
+class AppSettings(BaseSettings):
+
+    # Application
+    debug: bool
+    log_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, '.env'),
+        env_file_encoding='utf-8',
+        env_prefix='APP_',
+        extra='ignore',
+    )
+
+class Settings(BaseModel):
+
+    # Configuration settings
+    database: DatabaseSettings = DatabaseSettings()
+    # Application settings
+    app: AppSettings = AppSettings()
 
 settings = Settings()
